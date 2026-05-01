1af7628817a0e571c5faf34e96025bad2ad01331
Arshit rupani <125478383+arshitrupanii@users.noreply.github.com>
2026-05-01 14:00:19 +0530

    FINAL CHANGE

--- backend/lib/socket.js
+++ backend/lib/socket.js
@@ -5,11 +5,7 @@ import express from "express";
 const app = express();
 const server = http.createServer(app);
 
-const io = new Server(server, {
-  cors: {
-    origin: [process.env.FRONTEND_URL],
-  },
-});
+const io = new Server(server);
 
 export function getReceiverSocketId(userId) {
   return userSocketMap[userId];
